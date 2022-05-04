964ffd59868c17773db513f9e4549003d4ce71a6
m-eduard <marineduardconstantin2001@gmail.com>
2022-05-04 22:48:34 +0300

    Implemented server-side commands

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,40 +15,21 @@
 #include <unistd.h>
 
 #include "utils.h"
+#include "msg_transmission.h"
 
 using namespace std;
 
-#define MAX_CLIENTS 1000
 #define HOST_IP     "127.0.0.1"
 
 struct client_t {
-    int name;
+    string id_client;
+    bool active;
+
+    client_t() {}
+    client_t(string id) : id_client(id), active(true) {}
+    client_t(string id, bool x) : id_client(id), active(x) {}
 };
 
-void run_server(int tcp_sock, int udp_sock) {
-    long long bits_received = 0;
-
-    // int status = select(100000, read, write, NULL, NULL);
-}
-
-
-bool receive_msg(int sock_fd, char *message) {
-    bool status = true;
-
-    char buffer[MAX_LEN] = {0};
-    int sz = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
-
-    if (sz == 0) {
-        status = false;
-    } else {
-        uint32_t msg_len;
-        memcpy(&msg_len, buffer, MSG_LEN_SIZE);
-
-        memcpy(message, buffer + MSG_LEN_SIZE, msg_len);
-    }
-    
-    return status;
-}
 
 int main(int argc, char *argv[]) {
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
@@ -57,8 +39,9 @@ int main(int argc, char *argv[]) {
         return ERR;
     }
 
+    int ret;
     int udp_sock_fd, tcp_sock_fd, new_sock_fd;
-    char buffer[MAX_LEN];
+    char buffer[BUF_LEN];
     sockaddr_in serv_addr;
     sockaddr_in client_addr;
     socklen_t client_addr_size;
@@ -67,19 +50,33 @@ int main(int argc, char *argv[]) {
     fd_set tmp_fds;
     int fd_max;
 
+    // key:     socket file descriptor associated to client
+    // value:   data about the client
     unordered_map<int, client_t> clients;
+
+    // map every message to the tcp clients to the messages that it has to receive
+    // after reconnecting to the server
+    unordered_map<message, vector<client_t>> queued_messages();
+
+    // Set of id-s used by the clients that were
+    // connected to the server at least once
+    unordered_set<string> used_clients_ids;
+
+    // key:     string representing the topic name
+    // value:   2 lists of file descriptors associated to clients
+    //          that are subscribed to the given topic (one for
+    //          the clients with sf=0 on that topic, and one for
+    //          those with sf=1)
+    // unordered_map<string, pair<vector<int>, vector<int>>> topics;
+    // unordered_map<string, >
+
 
     // Open 2 sockets (one for UDP transmissions,
     // and one for the TCP connections)
-    if ((udp_sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        cerr << "Cannot create an UDP socket\n";
-        return ERR;
-    }
-
-    if ((tcp_sock_fd = socket(AF_INET, SOCK_STREAM, 0))< 0) {
-        cerr << "Cannot create a TCP socket\n";
-        return ERR;
-    }
+    udp_sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    DIE(udp_sock_fd < 0, "Cannot create an UDP socket");
+    tcp_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    DIE(tcp_sock_fd < 0, "Cannot create a TCP socket");
 
     // Deactivate the Nagle algorithm
     int flag = 1;
@@ -90,10 +87,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Check if the provided port is a valid int
-    if (atoi(argv[1]) == 0) {
-        cerr << "atoi() failed\n";
-        return ERR;
-    }
+    DIE(atoi(argv[1]) == 0, "atoi() failed");
 
     // Fill server info
     serv_addr = {
@@ -103,22 +97,15 @@ int main(int argc, char *argv[]) {
     };
 
     // Bind the sockets to a specified port
-    if (bind(udp_sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        cerr << "The port " << argv[1] << " is not available\n";
-        return ERR;
-    }
-
-    if (bind(tcp_sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        cerr << "The port " << argv[1] << " is not available\n";
-        return ERR;
-    }
+    ret = bind(udp_sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr));
+    DIE(ret < 0, "bind() failed");
+    ret = bind(tcp_sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr));
+    DIE(ret < 0, "bind() failed");
 
     // This socket is passive and it will listen to new connections
-    if (listen(tcp_sock_fd, MAX_CLIENTS) < 0) {
-        cerr << "listen() failed (maybe max number of "
-                "simultaneous connect requests was exceeded)\n";
-        return ERR;
-    }
+    ret = listen(tcp_sock_fd, MAX_CLIENTS);
+    DIE(ret < 0, "listen() failed (maybe max number of "
+                 "simultaneous connect requests was exceeded)");
 
     // Initialize the sets of file descriptors
     // (current fd-s: tcp_sock_fd and stdin fd)
@@ -129,19 +116,26 @@ int main(int argc, char *argv[]) {
 	fd_max = tcp_sock_fd;
 
     while (true) {
+        bool sig_exit = false;
         tmp_fds = read_fds;
 
-        if (select(fd_max + 1, &tmp_fds, NULL, NULL, NULL) < 0) {
-            cerr << "select() failed\n";
-            return ERR;
-        }
+        ret = select(fd_max + 1, &tmp_fds, NULL, NULL, NULL);
+        DIE(ret < 0, "select() failed");
 
         // Iterate through file descriptors to see
         // which one received data
         for (int i = 0; i < fd_max + 1; ++i) {
             if (FD_ISSET(i, &tmp_fds)) {
                 if (i == STDIN_FILENO) {
+                    memset(buffer, 0, sizeof(buffer));
+                    if (read(STDIN_FILENO, buffer, sizeof(buffer) - 1) < 0) {
+                        cerr << "read() failed\n";
+                    }
 
+                    if (!strncmp(buffer, "exit", 4)) {
+                        sig_exit = true;
+                        break;
+                    }
                 } else if (i == udp_sock_fd) {  // message from a UDP client
 
                 } else if (i == tcp_sock_fd) {  // connection request
@@ -159,22 +153,68 @@ int main(int argc, char *argv[]) {
                     fd_max = max(fd_max, new_sock_fd);
 
                     // Get the client ID
-                    if (!receive_msg(new_sock_fd, buffer)) {
-                        cerr << "skipped new client, since "
-                                "ID wasn't received)\n";
+                    ret = receive_message(new_sock_fd, buffer);
+                    if (ret < 0) {
+                        cerr << "Skipped new client, since the ID "
+                                "wasn't received\n";
                         continue;
                     }
 
-                    // Map the client to the file descriptor associated
-                    clients[new_sock_fd] = {1};
+                    string client_id(buffer);
 
-                    cout << "New client " << buffer << " connected from " << inet_ntoa(client_addr.sin_addr) << ":" << client_addr.sin_port << '\n';
+                    // Map the client to the file descriptor associated,
+                    // only if the id is not currently used
+                    if (connected_clients_id.find(client_id)
+                        == connected_clients_id.end())
+                    {
+                        cout << "New client " << client_id << " connected from "
+                             << inet_ntoa(client_addr.sin_addr) << ":"
+                             << client_addr.sin_port << '\n';
+                        send_message(new_sock_fd, ACC);
+
+                        connected_clients_id.insert(client_id);
+                        clients[new_sock_fd] = client_t(client_id);
+                    } else {
+                        cout << "Client " << client_id << " already connected.\n";
+                        send_message(new_sock_fd, REJ);
+                    }
                 } else {                        // message from a TCP client
+                    memset(buffer, 0, BUF_LEN);
+
+                    ret = receive_message(i, buffer);
+                    cout << "Ret is: " << ret << '\n';
+                    if (ret < 0) {
+                        cerr << "Receiving data from TCP client failed\n";
+                        continue;
+                    }
                     
+                    if (ret == 0) {
+                        clients.erase(i);
+                        cout << "Client disconnected";
+                        FD_CLR(i, &read_fds);
+                    } else {
+                        ret = receive_message(i, buffer);
+
+                        if (ret < 0) {
+                            cerr << "receive_message() failed\n";
+                            continue;
+                        }
+
+                        cout << buffer << '\n';;
+
+                        // cout << parse_server_msg(buffer);
+                    }
                 }
             }
         }
+
+        if (sig_exit) {
+            break;
+        }
     }
+
+
+
 
     close(tcp_sock_fd);
     close(udp_sock_fd);
--- subscriber.cpp
+++ subscriber.cpp
@@ -6,43 +6,23 @@
 #include <string.h>
 
 #include "utils.h"
+#include "msg_transmission.h"
 
 using namespace std;
 
-// 4 bytes of size, then the message itselfs
-struct message {
-    uint32_t size;
-    char payload[1500];
-};
-
-// Function that sends a given payload to a server
-// (connection to the server should be established
-//  before calling this function)
-bool send_message(int sock_fd, const char *message) {
-    bool status = true;
-    int ret;
-
-    char buffer[MAX_LEN];
-    uint32_t message_len = strlen(message) + 1;
-
-    memcpy(buffer, &message_len, MSG_LEN_SIZE);
-    snprintf(buffer + MSG_LEN_SIZE, strlen(message) + 1, "%s", message);
-
-    ret = send(sock_fd, buffer, MSG_LEN_SIZE + message_len, 0);
-    if (ret < 0) {
-        status = false;
-    }
-    
-    return status;
-}
+// // 4 bytes of size, then the message itselfs
+// struct message {
+//     uint32_t size;
+//     char payload[1500];
+// };
 
 // Message format: "subscribe <TOPIC> <SF>"
-string parse_subscribe_msg(char *msg) {    
+string parse_subscribe_cmd(char *msg) {    
     char cmd[MAX_CMD_LEN];
-    char topic[MAX_LEN / 2];
+    char topic[BUF_LEN / 2];
     int sf;
 
-    char parsed_msg[MAX_LEN] = {0};
+    char parsed_msg[BUF_LEN] = {0};
 
     if (sscanf(msg, "%s%s%d", cmd, topic, &sf) != 3) {
         cerr << "Subscribe format: \"subscribe <TOPIC> <SF>\"\n";
@@ -54,11 +34,11 @@ string parse_subscribe_msg(char *msg) {
 }
 
 // Message format: "unsubscribe <TOPIC>"
-string parse_unsubscribe_msg(char *msg) {
+string parse_unsubscribe_cmd(char *msg) {
     char cmd[MAX_CMD_LEN];
-    char topic[MAX_LEN / 2];
+    char topic[BUF_LEN / 2];
 
-    char parsed_msg[MAX_LEN] = {0};
+    char parsed_msg[BUF_LEN] = {0};
 
     if (sscanf(msg, "%s%s", cmd, topic) != 2) {
         cerr << "Unsubscribe format: \"unsubscribe <TOPIC>\"\n";
@@ -67,6 +47,19 @@ string parse_unsubscribe_msg(char *msg) {
     }
 
     return string(parsed_msg);
+}
+
+string parse_server_msg(char *msg) {
+    string parsed_msg;
+
+    uint32_t ip;
+    uint16_t port;
+
+    memcpy(&ip, msg, sizeof(ip));
+    memcpy(&port, msg + sizeof(ip), sizeof(port));
+    
+
+    return parsed_msg;
 }
 
 int main(int argc, char *argv[]) {
@@ -79,24 +72,19 @@ int main(int argc, char *argv[]) {
     }
 
     char *client_id = argv[1];
+    DIE(strlen(client_id) > MAX_ID_LEN, "Too long id");
 
+    int ret;
     int sock_fd;
     sockaddr_in serv_addr;
-    char buffer[MAX_LEN];
+    char buffer[BUF_LEN];
 
     fd_set read_fds;
     fd_set tmp_fds;
     int fd_max;
 
-    // protocolul meu:
-    // creez din server si trimit chunk-uri de
-    // 64 de bytes, iar pentru a finaliza un mesaj
-    // trimit o fereastra de 64 de bytes formati
-    // doar din biti de 1
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        cerr << "Cannot create a TCP socket\n";
-        return ERR;
-    }
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    DIE(sock_fd < 0, "Cannot create a TCP socket");
 
     // Deactivate the Nagle algorithm
     int flag = 1;
@@ -107,16 +95,10 @@ int main(int argc, char *argv[]) {
     }
 
     // Check if the provided port is a valid int
-    if (atoi(argv[3]) == 0) {
-        cerr << "atoi() failed\n";
-        return ERR;
-    }
+    DIE(atoi(argv[3]) == 0, "Invalid port");
 
     // Check the IP address
-    if (inet_addr(argv[2]) == 0) {
-        cerr << argv[2] << " is not a valid IP address\n";
-        return ERR;
-    }
+    DIE(inet_addr(argv[2]) == 0, "Invalid IP address");
 
     // Fill server info
     serv_addr = {
@@ -126,10 +108,8 @@ int main(int argc, char *argv[]) {
     };
 
     // Connect to the server
-    if (connect(sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        cerr << "Connection to the server at <" << argv[2] << "> failed\n";
-        return ERR;
-    }
+    ret = connect(sock_fd, (sockaddr *) &serv_addr, sizeof(serv_addr));
+    DIE(ret < 0, "Connection to the server failed");
 
     // Initialize the sets of file descriptors
     FD_ZERO(&read_fds);
@@ -139,51 +119,84 @@ int main(int argc, char *argv[]) {
     fd_max = sock_fd;
 
     // Send the client identification info to the server
-    if (!send_message(sock_fd, client_id)) {
-        cerr << "Sending client ID failed\n";
-        return ERR;
+    ret = send_message(sock_fd, client_id);
+    DIE(ret < 0, "Sending client ID failed");
+
+    // Receive the server response (accept or reject)
+    ret = receive_message(sock_fd, buffer);
+    DIE(ret < 0, "Receiving server confirmation failed");
+
+    if (strcmp(buffer, ACC)) {
+        close(sock_fd);
+        return 0;
     }
 
     while (true) {
         tmp_fds = read_fds;
 
-        if (select(fd_max + 1, &tmp_fds, NULL, NULL, NULL) < 0) {
-            cerr << "select() failed\n";
-            return ERR;
-        }
+        ret = select(fd_max + 1, &tmp_fds, NULL, NULL, NULL);
+        DIE(ret < 0, "select() failed");
 
-        // Either read from stdin (data provided by user),
+        // Read from stdin (data provided by user),
         // or from socket (data provided by server)
         if (FD_ISSET(STDIN_FILENO, &tmp_fds)) {
-            // fgets(buffer, MAX_LEN, stdin);
-
             memset(buffer, 0, sizeof(buffer));
-            if (read(STDIN_FILENO, buffer, sizeof(buffer) - 1) < 0) {
+            ret = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+            
+            if (ret < 0) {
                 cerr << "read() failed\n";
             }
 
             if (!strncmp(buffer, "exit", 4)) {
                 break;
             } else if (!strncmp(buffer, "subscribe ", 10)) {
-                string msg = parse_subscribe_msg(buffer);
+                string msg = parse_subscribe_cmd(buffer);
+                cout << msg << '\n';
 
                 if (msg.size() > 0) {
-                    if (send_message(sock_fd, msg.c_str()) < 0) {
-                        cerr << "send() failed in subscribe transmission\n";
+                    ret = send_message(sock_fd, msg.c_str());
+
+                    if (ret < 0) {
+                        cerr << "Sending subscribe cmd to the server failed\n";
                     } else {
                         cout << "Subscribed to topic.\n";
                     }
                 }
             } else if (!strncmp(buffer, "unsubscribe ", 12)) {
-                string msg = parse_unsubscribe_msg(buffer);
+                string msg = parse_unsubscribe_cmd(buffer);
 
                 if (msg.size() > 0) {
-                    if (send_message(sock_fd, msg.c_str()) < 0) {
-                        cerr << "send() failed in unsubscribe transmission\n";
+                    ret = send_message(sock_fd, msg.c_str());
+
+                    if (ret < 0) {
+                        cerr << "Sending unsubscribe cmd to the server failed";
+                        cerr << '\n';
                     } else {
                         cout << "Unsubscribed from topic.\n";
                     }
                 }
+            }
+        }
+        
+        if (FD_ISSET(sock_fd, &tmp_fds)) {
+            memset(buffer, 0, BUF_LEN);
+
+            ret = receive_message(sock_fd, buffer);
+            DIE(ret < 0, "Receiving server message failed");
+            
+            if (ret == 0) {
+                // Connection was closed
+                cout << "Server is down\n";
+                break;
+            } else {
+                ret = receive_message(sock_fd, buffer);
+
+                if (ret < 0) {
+                    cerr << "receive_message() failed\n";
+                    continue;
+                }
+
+                cout << parse_server_msg(buffer);
             }
         }
     }
