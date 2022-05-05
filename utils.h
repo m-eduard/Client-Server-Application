08443835ce9d258da0b2b3fa1563ea330cc78b63
m-eduard <marineduardconstantin2001@gmail.com>
2022-05-06 01:16:04 +0300

    Solved bug (&<union>.<structName> was blocking the execution)

--- utils.h
+++ utils.h
@@ -1,61 +1,88 @@
 #pragma once
 
-#define BUF_LEN         5000
-#define MAX_CLIENTS     50
-#define MSG_MAX_LEN		1600
-#define MSG_LEN_SIZE    4           // number of bytes to represent the message length
-#define MAX_CMD_LEN     20
-#define TOPIC_LEN	   	50
-#define MAX_DATA_LEN 	1500
-#define MAX_ID_LEN      10
-#define ERR             -1
+#define BUF_LEN         	5000
+#define MAX_CLIENTS     	50
+#define MSG_MAX_LEN			1600
+#define MSG_LEN_SIZE    	4           // number of bytes to represent the message length
+#define MAX_CMD_LEN     	20
+#define TOPIC_LEN	   		50
+#define MAX_DATA_LEN 		1500
+#define MAX_ID_LEN      	10
+#define ERR             	-1
 
-#define INT             0
-#define SHORT_REAL      1
-#define FLOAT           2
-#define STRING          3
+#define INT             	0
+#define SHORT_REAL      	1
+#define FLOAT           	2
+#define STRING          	3
 
-#define ACC             "ACC"
-#define REJ             "REJ"
-// #define END				"END"
+#define INTEGER_T_SZ		40			// Sizes of my custom data types
+#define SHORT_REAL_T_SZ		8			// used in struct message
+#define FLOAT_T_SZ 			48
 
-// struct __attribute__((packed)) message {
-struct message {
+#define ACC             	"ACC"
+#define REJ             	"REJ"
+
+struct message_t {
     uint32_t ip;
     uint16_t port;
     uint8_t type;
 	char topic[TOPIC_LEN + 1];
 
-	// union {
-	// 	struct integer_t {
-	// 		int8_t sign;
-	// 		uint32_t num;
-	// 	};
+	union {
+		struct __attribute__ ((__packed__)) {
+			int8_t sign;
+			uint32_t num;
+		} integer_t;
 
-	// 	int16_t short_real_t;
+		int16_t short_real_t;
 
-	// 	struct float_t {
-	// 		int8_t sign;
-	// 		uint32_t decimal;
-	// 		uint8_t fractional;
-	// 	};
+		struct __attribute__ ((__packed__)) {
+			int8_t sign;
+			uint32_t decimal;
+			uint8_t fractional;
+		} float_t;
 
-	// 	char *string_t;
-	// } data;
+		char *string_t;
+	} data;
 
-    message() {}
-	// message(uint32_t ip, uint16_t port, uint8_t type, char *topic) : ip(ip), port(port),
-	// 													type(type) {
-	// 														this->ip = ip;
-	// 														this->port = port;
-	// 														this->type = type;
-	// 														memcpy(this->topic, topic, TOPIC_LEN);}
-	// message(uint32_t ip, uint16_t port, uint8_t type,
-	// 		)
-	// {
-	// 	message(ip, port, type);
-	// 	memcpy(this->topic, topic, TOPIC_LEN);
-	// }
+
+    message_t() {}
+	message_t(uint32_t ip, uint16_t port, uint8_t type) :
+		ip(ip), port(port), type(type) {}
+	
+	message_t(uint32_t ip, uint16_t port, uint8_t type,
+			char *topic, char *data) : message_t(ip, port, type)
+	{
+		memcpy(this->topic, topic, TOPIC_LEN);
+
+		char *tmp = (char *)calloc(MAX_DATA_LEN + 1, sizeof(char));
+
+		switch(type) {
+			case INT:
+				memcpy(tmp, data, INTEGER_T_SZ);
+
+				memcpy(&this->data.integer_t.sign, tmp += 1, 1);
+				memcpy(&this->data.integer_t.num, tmp, 4);
+
+				break;
+			case SHORT_REAL:
+				memcpy(&this->data.short_real_t, data, SHORT_REAL_T_SZ);
+				break;
+			case FLOAT:
+				memcpy(tmp, data, FLOAT_T_SZ);
+
+				memcpy(&this->data.float_t.sign, tmp += 1, 1);
+				memcpy(&this->data.float_t.decimal, tmp += 4, 4);
+				memcpy(&this->data.float_t.fractional, tmp, 1);
+
+				break;
+			case STRING:
+				memcpy(tmp, data, MAX_DATA_LEN);
+
+				this->data.string_t = tmp;
+				break;
+		}
+	}
 };
 
 #define DIE(assertion, call_description)	\
